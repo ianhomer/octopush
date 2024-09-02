ed99f9e4fcb10db45b90e49448094cbd3ec9f6d0
Ian Homer <code@ianhomer.com>
2024-09-02 15:53:57 +0100

    ✨ list consumption

--- README.md
+++ README.md
@@ -4,6 +4,30 @@ Playing with [Octopus energy API](https://developer.octopus.energy/rest/referenc
 
 Set environment variables, e.g. with [direnv](https://direnv.net/).
 
+| Variable              |                                                      |
+| --------------------- | ---------------------------------------------------- |
+| OCTOPUS_MPAN          | MPAN - Meter Point Administration Number             |
+| OCTOPUS_SERIAL_NUMBER | Meter serial number                                  |
+| OCTOPUS_API_KEY       | API Key from your profile page in the Octopus portal |
+
+Install
+
     pip install -e .
 
+And get consumption
 
+```sh
+❯ octopush --list-consumption
+2024-08-30T23:00:00+01:00    0.114
+2024-08-30T23:30:00+01:00    0.085
+2024-08-31T00:00:00+01:00    0.046
+2024-08-31T00:30:00+01:00    0.045
+2024-08-31T01:00:00+01:00    0.096
+                             ...
+2024-09-01T22:30:00+01:00    0.214
+2024-09-01T23:00:00+01:00    0.073
+2024-09-01T23:30:00+01:00    0.076
+2024-09-02T00:00:00+01:00    0.098
+2024-09-02T00:30:00+01:00    0.060
+Length: 100, dtype: float64
+```
--- octopush.py
+++ octopush.py
@@ -7,12 +7,14 @@ import os
 
 parser = argparse.ArgumentParser(description="octopush")
 parser.add_argument("--mpan", help="Specify MPAN")
-parser.add_argument(
-    "--get-meter-point", help="Get meter point", action="store_true"
-)
+parser.add_argument("--serial-number", help="Specify meter serial number")
+parser.add_argument("--get-meter-point", help="Get meter point", action="store_true")
+parser.add_argument("--list-consumption", help="List consumption", action="store_true")
 parser.add_argument("--list-products", help="List products", action="store_true")
 
 OCTOPUS_MPAN_NAME = "OCTOPUS_MPAN"
+OCTOPUS_SERIAL_NUMBER_NAME = "OCTOPUS_SERIAL_NUMBER"
+OCTOPUS_API_KEY_NAME = "OCTOPUS_API_KEY"
 
 
 def main():
@@ -25,6 +27,8 @@ def do(args):
         list_products()
     elif args.get_meter_point:
         get_meter_point(args)
+    elif args.list_consumption:
+        list_consumption(args)
     else:
         parser.print_help()
         sys.exit(0)
@@ -48,19 +52,58 @@ def list_products():
     print(products)
 
 
-def get_meter_point(args):
+def get_mpan(args):
     try:
-        mpan = args.mpan or os.environ[OCTOPUS_MPAN_NAME]
+        return args.mpan or os.environ[OCTOPUS_MPAN_NAME]
     except KeyError:
         print(
             "Please set mpan with argument --mpan "
             + f"or environment variable {OCTOPUS_MPAN_NAME}\n"
         )
         raise Exception("mpan not specified")
+
+
+def get_serial_number(args):
+    try:
+        return args.serial_number or os.environ[OCTOPUS_SERIAL_NUMBER_NAME]
+    except KeyError:
+        print(
+            "Please set serial_number with argument --serial-number "
+            + f"or environment variable {OCTOPUS_SERIAL_NUMBER_NAME}\n"
+        )
+        raise Exception("serial number not specified")
+
+
+def get_api_key():
+    try:
+        return os.environ[OCTOPUS_API_KEY_NAME]
+    except KeyError:
+        print("Please set " + f"environment variable {OCTOPUS_API_KEY_NAME}\n")
+        raise Exception("api key not specified")
+
+
+def get_meter_point(args):
+    mpan = get_mpan(args)
     url = f"https://api.octopus.energy/v1/electricity-meter-points/{mpan}/"
     response = requests.get(url=url)
     json = response.json()
     print(json)
+
+
+def list_consumption(args):
+    mpan = get_mpan(args)
+    serial_number = get_serial_number(args)
+    api_key = get_api_key()
+    url = (
+        "https://api.octopus.energy/v1/electricity-meter-points"
+        + f"/{mpan}/meters/{serial_number}/consumption/"
+    )
+    response = requests.get(url=url, auth=(api_key, ""))
+    json = response.json()
+    interval_start = [x['interval_start'] for x in json['results']]
+    consumption = [x['consumption'] for x in json['results']]
+    usage = pandas.Series(consumption, index=interval_start).sort_index()
+    print(usage)
 
 
 main()
