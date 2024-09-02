727fe2418f5dd4352b1fbc2a12cbd782b2836022
Ian Homer <code@ianhomer.com>
2024-09-02 15:19:34 +0100

    Get meter point

--- octopush.py
+++ octopush.py
@@ -3,9 +3,16 @@ import requests
 import sys
 import pandas
 from datetime import datetime
+import os
 
 parser = argparse.ArgumentParser(description="octopush")
+parser.add_argument("--mpan", help="Specify MPAN")
+parser.add_argument(
+    "--get-meter-point", help="Get meter point", action="store_true"
+)
 parser.add_argument("--list-products", help="List products", action="store_true")
+
+OCTOPUS_MPAN_NAME = "OCTOPUS_MPAN"
 
 
 def main():
@@ -16,6 +23,8 @@ def main():
 def do(args):
     if args.list_products:
         list_products()
+    elif args.get_meter_point:
+        get_meter_point(args)
     else:
         parser.print_help()
         sys.exit(0)
@@ -37,6 +46,21 @@ def list_products():
     }
     products = pandas.DataFrame(data).set_index("From").sort_index()
     print(products)
+
+
+def get_meter_point(args):
+    try:
+        mpan = args.mpan or os.environ[OCTOPUS_MPAN_NAME]
+    except KeyError:
+        print(
+            "Please set mpan with argument --mpan "
+            + f"or environment variable {OCTOPUS_MPAN_NAME}\n"
+        )
+        raise Exception("mpan not specified")
+    url = f"https://api.octopus.energy/v1/electricity-meter-points/{mpan}/"
+    response = requests.get(url=url)
+    json = response.json()
+    print(json)
 
 
 main()
